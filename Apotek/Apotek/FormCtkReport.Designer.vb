363816c64f0e05d6808481ab2d477db15e44111b
jasonloardy <jasonloardy_17@kharisma.ac.id>
2019-05-14 19:20:10 +0800

    13 05 2019

---
+++ Apotek/Apotek/FormCtkReport.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormCtkReport
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.dgvpenjualan = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dgvdetail = New System.Windows.Forms.DataGridView()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.dgvpenjualan, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.dgvdetail, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(695, 326)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Cetak"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'dgvpenjualan
+        '
+        Me.dgvpenjualan.AllowUserToAddRows = False
+        Me.dgvpenjualan.AllowUserToDeleteRows = False
+        Me.dgvpenjualan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvpenjualan.Location = New System.Drawing.Point(6, 45)
+        Me.dgvpenjualan.Name = "dgvpenjualan"
+        Me.dgvpenjualan.Size = New System.Drawing.Size(240, 257)
+        Me.dgvpenjualan.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.dgvpenjualan)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(252, 308)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Daftar Transaksi"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Cari ID Penjualan :"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(107, 19)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(139, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.dgvdetail)
+        Me.GroupBox2.Location = New System.Drawing.Point(270, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(506, 308)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Detail Obat"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "ID Penjualan :"
+        '
+        'dgvdetail
+        '
+        Me.dgvdetail.AllowUserToAddRows = False
+        Me.dgvdetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvdetail.Location = New System.Drawing.Point(6, 45)
+        Me.dgvdetail.Name = "dgvdetail"
+        Me.dgvdetail.Size = New System.Drawing.Size(494, 237)
+        Me.dgvdetail.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(9, 285)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(491, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Grand Total : Rp. 999,999,999"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        '
+        'FormCtkReport
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(789, 361)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "FormCtkReport"
+        Me.Text = "FormCtkReport"
+        CType(Me.dgvpenjualan, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.dgvdetail, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents dgvpenjualan As System.Windows.Forms.DataGridView
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents dgvdetail As System.Windows.Forms.DataGridView
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+End Class
---
+++ Apotek/Apotek/FormCtkReport.vb
@@ -0,0 +1,107 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class FormCtkReport
+    Public id_data As String
+    Public cari As String
+
+    Private Sub FormCtkReport_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        isigridpenj()
+    End Sub
+    Sub isigridpenj()
+        Dim query As String = "SELECT * FROM penjualan WHERE id_penjualan LIKE '%" & cari & "%' ORDER BY tanggal DESC"
+        Dim da As New MySqlDataAdapter(query, konek)
+        Dim ds As New DataSet()
+        If da.Fill(ds) Then
+            dgvpenjualan.DataSource = ds.Tables(0)
+            dgvpenjualan.Refresh()
+        End If
+        If dgvpenjualan.RowCount > 0 Then
+            judulgridpenj()
+        End If
+    End Sub
+
+    Sub judulgridpenj()
+        Dim objAlternatingCellStyle As New DataGridViewCellStyle()
+        dgvpenjualan.AlternatingRowsDefaultCellStyle = objAlternatingCellStyle
+        Dim style As DataGridViewCellStyle = dgvpenjualan.Columns(0).DefaultCellStyle
+        dgvpenjualan.Columns(0).HeaderText = "ID Penjualan"
+        dgvpenjualan.Columns(1).HeaderText = "Tanggal"
+        dgvpenjualan.Columns(0).Width = 100
+        dgvpenjualan.Columns(1).Width = 75
+        objAlternatingCellStyle.BackColor = Color.AliceBlue
+        dgvpenjualan.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        dgvpenjualan.ReadOnly = True
+    End Sub
+
+    Sub isigriddetail()
+        Dim query As String = "SELECT pj.id_obat,o.nama_obat,pj.harga,pj.qty,pj.harga*pj.qty FROM penjualan_detail pj JOIN obat o ON pj.id_obat=o.id_obat WHERE id_penjualan='" & id_data & "'"
+        Dim da As New MySqlDataAdapter(query, konek)
+        Dim ds As New DataSet()
+        If da.Fill(ds) Then
+            dgvdetail.DataSource = ds.Tables(0)
+            dgvdetail.Refresh()
+        End If
+        If dgvpenjualan.RowCount > 0 Then
+            judulgriddetail()
+        End If
+    End Sub
+
+    Sub judulgriddetail()
+        Dim objAlternatingCellStyle As New DataGridViewCellStyle()
+        dgvdetail.AlternatingRowsDefaultCellStyle = objAlternatingCellStyle
+        Dim style As DataGridViewCellStyle = dgvdetail.Columns(0).DefaultCellStyle
+        dgvdetail.Columns(0).HeaderText = "ID Obat"
+        dgvdetail.Columns(1).HeaderText = "Nama Obat"
+        dgvdetail.Columns(2).HeaderText = "Harga"
+        dgvdetail.Columns(3).HeaderText = "Qty"
+        dgvdetail.Columns(4).HeaderText = "Subtotal"
+        dgvdetail.Columns(0).Width = 75
+        dgvdetail.Columns(1).Width = 150
+        dgvdetail.Columns(2).Width = 75
+        dgvdetail.Columns(3).Width = 50
+        dgvdetail.Columns(4).Width = 75
+
+        objAlternatingCellStyle.BackColor = Color.AliceBlue
+        dgvdetail.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        dgvdetail.ReadOnly = True
+    End Sub
+    Sub hitunggrandtotal()
+        Dim total As Integer
+        For index As Integer = 0 To dgvdetail.RowCount - 1
+            total += Convert.ToInt32(dgvdetail.Rows(index).Cells(4).Value)
+        Next
+        Label3.Text = "Grand Total = Rp. " & FormatNumber(total, 0)
+    End Sub
+
+
+    Sub isidetail()
+        dgvpenjualan.Refresh()
+        If dgvpenjualan.RowCount > 0 Then
+            Dim baris As Integer
+            With dgvpenjualan
+                baris = .CurrentRow.Index
+                id_data = .Item(0, baris).Value
+                Label2.Text = "ID Penjualan : " & .Item(0, baris).Value
+                isigriddetail()
+                hitunggrandtotal()
+            End With
+        End If
+    End Sub
+
+    Private Sub dgvpenjualan_CellEnter(sender As Object, e As DataGridViewCellEventArgs) Handles dgvpenjualan.CellEnter
+        isidetail()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        cari = TextBox1.Text
+        isigridpenj()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim baris As Integer = dgvpenjualan.CurrentRow.Index
+        Dim cno As String = dgvpenjualan.Item(0, baris).Value
+        FormCtkNota.CRNota1.RecordSelectionFormula = "{penjualan1.id_penjualan} = '" & cno & "'"
+        FormCtkNota.CRNota1.Refresh()
+        FormCtkNota.Show()
+    End Sub
+End Class
